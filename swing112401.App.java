e2befa477a3e975a0326788316ce316bf8b548c5
ss0477703 <74774401+ss0477703@users.noreply.github.com>
2020-11-24 20:56:02 +0800

    课堂练习

---
+++ swing112401.App.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class aa01 {
+    JPanel myPanel=new JPanel();
+    JLabel label_txt=new JLabel();
+    JTextArea textArea_A=new JTextArea();
+    JTextArea textArea_B=new JTextArea();
+    //构造方法
+    public aa01() {
+
+        textArea_A.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+
+                if (e.getKeyCode()==KeyEvent.VK_UP){
+                    textArea_B.append("欢迎进入Java课程\n");
+                };
+                if (e.getKeyCode()==KeyEvent.VK_DOWN){
+                    textArea_B.setText("");
+                };
+
+            }
+        });
+
+
+
+    }
+    //显示窗体方法
+    void go(){
+        myPanel.add(textArea_A);
+        myPanel.add(textArea_B);
+        textArea_A.setBounds(300,300,500,100);
+        textArea_B.setBounds(300,500,500,100);
+        textArea_A.setBackground(Color.cyan);
+        textArea_B.setBackground(Color.pink);
+
+        JFrame frame = new JFrame("TestGUI");
+        myPanel.setLayout(null);
+        myPanel.add(label_txt);
+        frame.setContentPane(myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100,100,1000,1000);
+        //frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new aa01().go();
+    }
+}
