8e8f90fdaf78b2e9c7d08738fbe8e9cb3a23dabb
ss0477703 <74774401+ss0477703@users.noreply.github.com>
2020-11-30 15:17:32 +0800

    继承

---
+++ 113001继承父类.R.java
@@ -0,0 +1,13 @@
+package pac;
+
+public class App  extends R{
+
+	public static void main(String[] args) {
+
+		System.out.println(boy_filePanth);
+		System.out.println(girl_filePanth);
+		System.out.println(array2d[0][1]+","+array2d[1][0]);
+		
+	}
+
+}
